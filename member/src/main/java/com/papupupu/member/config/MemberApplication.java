e5eed70a01f21ac7ac2cbee170240fb55f36002f
papupupu <zx2910935654@gmail.com>
2024-03-05 12:29:54 +0800

    修改gitignore文件，并且增加日志

--- member/src/main/java/com/papupupu/member/config/MemberApplication.java
+++ member/src/main/java/com/papupupu/member/config/MemberApplication.java
@@ -1,15 +1,24 @@
 package com.papupupu.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan("com.papupupu")
 public class MemberApplication {
 
+    private  static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        LOG.info("启动成功‼");
+            LOG.info("地址: \thttp://localhost:{}", environment.getProperty("server.port"));
+//        SpringApplication.run(MemberApplication.class, args);
     }
 
 }
